adc3859ea05f047e2410f65368a12075b8fef677
ward <bonuswon@gmail.com>
2022-11-24 12:29:21 +0900

    init project

---
+++ src/main/java/com/example/springcoreadvanced/SpringcoreAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcoreadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoreAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcoreAdvancedApplication.class, args);
+    }
+
+}
